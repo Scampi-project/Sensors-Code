b9c5dc4b2b0227d79b593786bd8da4595cf2a237
BenjaminBFP <166150574+BenjaminBFP@users.noreply.github.com>
2024-08-04 12:21:25 +0200

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -1,9 +1,17 @@
-import board
-import analogio
+#import board
+# import analogio
+import sys
+# ~ import analogio #Ne fonctionne pas pour sur le raspberry 3B, impossible d'importer la librairie
+sys.path.insert(1,"/home/pi/SCAMPI/Management") #Utils is in another folder have to give the path
+from Utils import Logger
 
-tmp36 = analogio.AnalogIn(board.A0)
-
-def get_temperature():
-    voltage = tmp36.value * 3.3 / 65536
-    temperature = (voltage - 0.5) * 100
-    return temperature
+class Temperature:    
+    def __init__(self):
+        self.log = Logger()
+        #self.tmp36 = analogio.AnalogIn(board.A0)
+    def get_temperature(self):
+        self.temperature = 25
+        #self.voltage = self.tmp36.value * 3.3 / 65536
+        #self.temperature = (self.voltage - 0.5) * 100
+        self.log.log_values("tmp36_temperature",self.temperature)
+    
