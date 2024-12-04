f1dd8039ce06835027682d7f8498f3906ee5fa8f
viren-ollivier <gael.ollivier03@gmail.com>
2024-12-04 14:02:17 +0100

    Add files via upload

--- camera.py
+++ camera.py
@@ -1,20 +1,24 @@
-#Author : Benjamin PONTY(class version, improvement/updating), Original code : Guillaume Soulier
+#Author : Benjamin PONTY(class version, improvement/updating), Original code : Guillaume Soulier, modified by : Viren OLLIVIER
 
 from time import sleep
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
+from libcamera import controls
 from datetime import datetime
 import sys
-sys.path.insert(1,"/home/pi/SCAMPI/Management") # import Utils file that is in another file
+sys.path.insert(1,"/home/pi1/SCAMPI/Management") # import Utils file that is in another file
 from Utils import Logger                     # have to modify the path if you change the organisation
  
 # Ignition   
 class Camera: 
     def __init__(self) :
         self.camera = Picamera2()
+        self.config = self.camera.create_video_configuration()
+        self.camera.configure(self.config)
         self.brightness_index = 100
         self.encoder = H264Encoder(10000000)
         self.logger = Logger()
+    """   #inutile
     def set_up(self):
         self.logger.log_info("measurement_operations",'camera detected')
         self.camera.exposure_mode = 'auto' #exposure
@@ -36,11 +40,11 @@ class Camera:
         self.brightness_index = 100
         self.camera.brightness = self.brightness_index
         self.logger.log_info("measurement_operations",'self.camera brigthness set to default')
-        sleep(5)
+        #sleep(5)
         self.camera.stop_preview()
         self.logger.log_info("measurement_operations",'self.camera off')
 
-
+"""
 
     # Standard Functions
 
@@ -52,23 +56,28 @@ class Camera:
         self.camera.start_preview(alpha=200)
         self.logger.log_info("measurement_operations",'picture transparency set to 200')
 
-    def capture_photo(self,path) :
+    def capture_photo(self) :
         self.camera.start_preview() #camera turn on
         self.logger.log_info("measurement_operations",'camera on')
         self.logger.log_info("measurement_operations",'camera focusing')
-        sleep(5) #focus time
+        #sleep(5) #focus time
         self.camera.start()
-        
-        self.camera.capture_file(path) #create a file indexed by the measurement name (ex file name : Photo31-01-2024-14:52)
+        self.camera.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+        sleep(2)
+        self.camera.capture_file(f'/home/pi1/SCAMPI/Sensors/photo{datetime.now()}.jpg') #create a file indexed by the measurement name (ex file name : Photo31-01-2024-14:52)
         self.camera.stop_preview() #camera turn off
         self.logger.log_info("measurement_operations",'camera off')
 
-    def capture_video(self,path) :
+    def capture_video(self,Long=False) :
         self.camera.start_preview()
         self.logger.log_info("measurement_operations",'camera on')
-        self.camera.start_recording(self.encoder,path)
+        self.camera.start_recording(self.encoder,f'/home/pi1/SCAMPI/Sensors/video{datetime.now()}.h264')
+        self.camera.set_controls({"AfMode": controls.AfModeEnum.Continuous , "AfSpeed" : controls.AfSpeedEnum.Fast})
         self.logger.log_info("measurement_operations",'camera recording')
-        sleep(30) #video de 30 secondes
+        if Long:
+                sleep(60)#video d'une min
+        else:
+                sleep(15) #video de 15 secondes
         self.camera.stop_recording()
         self.logger.log_info("measurement_operations",'stop recording')
         self.camera.stop_preview()
@@ -95,6 +104,5 @@ class Camera:
         print('camera off')
 if __name__ == '__main__':
     test = Camera()
-    test.set_up()
-    #test.capture_photo(f'/home/pi/SCAMPI/Sensors/photo{datetime.now()}.jpg')
-    test.capture_video(f'/home/pi/SCAMPI/Sensors/video{datetime.now()}.h264')
+    test.capture_photo()
+    #test.capture_video()
