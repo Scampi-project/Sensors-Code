fe2d91dd4bbd734e667b16a8f82845d146e9bfb7
BenjaminBFP <166150574+BenjaminBFP@users.noreply.github.com>
2025-01-31 14:32:54 +0100

    Update led_panel.py
    Modification de la manière d'allumer les LED pour être en adéquation avec les changements des autres programmes.
    Avant l'initialisation lançait automatiquement les LED. Maintenant, il faut utiliser _turn-on_

--- led_panel.py
+++ led_panel.py
@@ -7,36 +7,32 @@ import sys
 sys.path.insert(1,"/home/pi/SCAMPI/Management") #Utils is in another folder have to give the path
 from Utils import Logger
 class Led_panel:
-    def __init__(self,temperature):#should add the temperature from the aquarium
+    def __init__(self):#should add the temperature from the aquarium
         self.logger = Logger()
-        self.temp = temperature
         self.human_luminosity_control = False
         GPIO.setmode(GPIO.BCM)
-        #self.gpio_luminosity_pin = 18  # Pin for the photocell that checks if LEDs are working
-        self.gpio_day_led_pin = 17     # Pin for the Day LED transistor
-        #self.gpio_emergency_led_pin = 23    # Pin for the Emergency LED transistor
-        #GPIO.setup(self.gpio_luminosity_pin, GPIO.IN)  # Input mode for LDR
+        self.gpio_luminosity_pin = 18  # Pin for the photocell that checks if LEDs are working
+        self.gpio_day_led_pin = 1     # Pin for the Day LED transistor
+        self.gpio_emergency_led_pin = 23    # Pin for the Emergency LED transistor
+        GPIO.setup(self.gpio_luminosity_pin, GPIO.IN)  # Input mode for LDR
         GPIO.setup(self.gpio_day_led_pin, GPIO.OUT)     # Output mode for Day LED
-        #GPIO.setup(self.gpio_emergency_led_pin, GPIO.OUT)   # Output mode for Emergency LED
-        self.current_hour = datetime.now().hour
-
-    def manage_leds(self):
-
-        if 8 <= self.current_hour < 20:  # Between 8am and 8pm
-            GPIO.output(self.gpio_day_led_pin, GPIO.HIGH)  # Turn on Day LED
-
-            if GPIO.input(self.gpio_luminosity_pin) == GPIO.LOW and self.temp<26 and self.human_luminosity_control == False:  # If low luminosity
-                GPIO.output(self.gpio_emergency_led_pin, GPIO.HIGH)  # Turn on Emergency LED
-                self.logger.log_info("power_operations","Emergency LED turn ON, luminosity is too low")
-            else:
-                GPIO.output(self.gpio_emergency_led_pin, GPIO.LOW) # Turn off Emergency LED
-                self.logger.log_info("power_operations"," luminosity is fine")
+        GPIO.setup(self.gpio_emergency_led_pin, GPIO.OUT)   # Output mode for Emergency LED
+    def turn_on(self):
+        GPIO.output(self.gpio_day_led_pin, GPIO.HIGH)  # Turn on Day LED
+    def check_luminosity(self,temp):
+        if GPIO.input(self.gpio_luminosity_pin) == GPIO.LOW and temp<26 and self.human_luminosity_control == False:  # If low luminosity have to emmit an alert 
+            GPIO.output(self.gpio_emergency_led_pin, GPIO.HIGH)  # Turn on Emergency LED                                    But have to check if can't add a luminosity sensor
+            self.logger.log_info("power_operations","Emergency LED turn ON, luminosity is too low")
         else:
-            GPIO.output(self.gpio_day_led_pin, GPIO.LOW)   # Turn off Day LED
             GPIO.output(self.gpio_emergency_led_pin, GPIO.LOW) # Turn off Emergency LED
+            self.logger.log_info("power_operations"," luminosity is fine")
+            pass
+    def turn_off(self):
+        GPIO.output(self.gpio_day_led_pin, GPIO.LOW)   # Turn off Day LED
+        GPIO.output(self.gpio_emergency_led_pin, GPIO.LOW) # Turn off Emergency LED
     def reduce_temperature(self,temperature):
         while temperature>26.1: # 0.1 in more to make sure that the condition for the emergency led isn't met
-                try :
+                try :           #Have to check if the code is still working or stopping
                     #lowering luminosity of the led panel by half
                     GPIO.output(self.gpio_day_led_pin, GPIO.HIGH)
                     time.sleep(0.005)
@@ -58,22 +54,14 @@ class Led_panel:
                 except KeyboardInterrupt:
                     GPIO.cleanup()
                     break
+            self.manage_leds()
             
                             
             
         #GPIO.cleanup() have to cleanup led if during the shutdown sequence
 if __name__ == "__main__":
-    test = Led_panel(15)
-    test.reduce_luminosity(100, 5)
-    print("80%")
-    test.reduce_luminosity(80, 5)
-    print("60%")
-    test.reduce_luminosity(60, 5)
-    print("40%")
-    test.reduce_luminosity(40, 5)
-    print("20%")
-    test.reduce_luminosity(20, 5)
-
+    test = Led_panel()
+    test.turn_on()
             
             
             
